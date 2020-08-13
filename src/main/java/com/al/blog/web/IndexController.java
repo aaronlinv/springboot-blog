529a067dc451df0d8425837832606821ebdf5713
Aaron <1546848781@qq.com>
2020-08-13 21:18:01 +0800

    修改css和图片链接

--- src/main/java/com/al/blog/web/IndexController.java
+++ src/main/java/com/al/blog/web/IndexController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable("id") Integer id, @PathVariable("name") String name){
+    @GetMapping("/")
+    public String index(){
 /*        // int i= 10/0;
         String blog= null;
         if(blog == null){
