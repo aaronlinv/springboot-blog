0e3bd4ebe19bf8ed69239498fbbce76d8c120e19
Aaron <1546848781@qq.com>
2020-08-20 15:42:41 +0800

    UserServiceImpl

--- src/main/java/com/al/blog/service/UserServiceImpl.java
+++ src/main/java/com/al/blog/service/UserServiceImpl.java
@@ -1,12 +1,19 @@
 package com.al.blog.service;
 
+import com.al.blog.dao.UserRepository;
 import com.al.blog.po.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+    
     @Override
     public User checkUser(String username, String password) {
-        
-        
-        return null;
+
+        User user = userRepository.findByUsernameandAndPassword(username, password);
+        return user;
     }
 }
