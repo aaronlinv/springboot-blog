6aefcdbf0263caa0cf04de3888adfbf3a0768658
Aaron <1546848781@qq.com>
2020-08-20 15:40:28 +0800

    UserRepository

---
+++ src/main/java/com/al/blog/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.al.blog.dao;
+
+import com.al.blog.po.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+// <Dao操作的对象，主键类型>
+public interface UserRepository extends JpaRepository<User,Long> {
+    
+}
