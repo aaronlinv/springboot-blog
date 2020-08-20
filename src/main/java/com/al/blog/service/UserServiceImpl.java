580e005955c47b4676409c86a094ccd524227258
Aaron <1546848781@qq.com>
2020-08-20 16:49:54 +0800

    MD5加密

--- src/main/java/com/al/blog/service/UserServiceImpl.java
+++ src/main/java/com/al/blog/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.al.blog.dao.UserRepository;
 import com.al.blog.po.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -13,7 +14,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User checkUser(String username, String password) {
 
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, DigestUtils.md5DigestAsHex(password.getBytes()));
         return user;
     }
 }
--- src/test/java/com/al/blog/BlogApplicationTests.java
+++ src/test/java/com/al/blog/BlogApplicationTests.java
@@ -2,12 +2,15 @@ package com.al.blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.DigestUtils;
 
 @SpringBootTest
 class BlogApplicationTests {
 
     @Test
     void contextLoads() {
+        String md5DigestAsHex = DigestUtils.md5DigestAsHex("123456".getBytes());
+        System.out.println(md5DigestAsHex);
     }
 
 }
