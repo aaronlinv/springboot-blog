fb2d2cd0e785b54b318d1a5d4feb8dba46329dfd
Aaron <1546848781@qq.com>
2020-08-22 14:00:18 +0800

    新增Type 修改input页面错误

--- src/main/java/com/al/blog/web/admin/TypeController.java
+++ src/main/java/com/al/blog/web/admin/TypeController.java
@@ -1,5 +1,6 @@
 package com.al.blog.web.admin;
 
+import com.al.blog.po.Type;
 import com.al.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,5 +30,16 @@ public class TypeController {
     @GetMapping("/types/input")
     public String input(){
         return "admin/types-input";
+    }
+    
+    @PostMapping("/types")
+    public String post(Type type){
+        Type t = typeService.save(type);
+        if (t == null) {
+            
+        }else{
+            
+        }
+        return "redirect:/admin/types";
     }
 }
