5b0f7177c4b260c4cb9049cf0b72a49beab38614
Aaron <1546848781@qq.com>
2020-08-22 11:43:29 +0800

    TypeServiceImpl

---
+++ src/main/java/com/al/blog/dao/TypeRepository.java
@@ -0,0 +1,8 @@
+package com.al.blog.dao;
+
+import com.al.blog.po.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository<Type,Long> {
+    
+}
