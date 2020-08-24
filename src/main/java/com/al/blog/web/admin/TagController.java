9b20f447a7e8679f3358bf9e7fb543631d59c469
Aaron <1546848781@qq.com>
2020-08-24 09:49:34 +0800

    完成Tag删除

--- src/main/java/com/al/blog/web/admin/TagController.java
+++ src/main/java/com/al/blog/web/admin/TagController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -56,5 +57,12 @@ public class TagController {
         }
         return "redirect:/admin/tags";
     }
+    @GetMapping("/tags/{id}/delete")
+    public String deleteTag(@PathVariable Long id,RedirectAttributes attributes){
+        tagService.deleteTag(id);
+        attributes.addFlashAttribute("message","删除成功");
+        return "redirect:/admin/tags";
+    }
+    
     
 }
