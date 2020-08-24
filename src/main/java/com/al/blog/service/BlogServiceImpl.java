df6ed56e5dbd345bf05b9abf71f54184eb18b321
Aaron <1546848781@qq.com>
2020-08-24 10:41:44 +0800

    博客动态查询

--- src/main/java/com/al/blog/service/BlogServiceImpl.java
+++ src/main/java/com/al/blog/service/BlogServiceImpl.java
@@ -3,11 +3,20 @@ package com.al.blog.service;
 import com.al.blog.NotFoundException;
 import com.al.blog.dao.BlogRepository;
 import com.al.blog.po.Blog;
+import com.al.blog.po.Type;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BlogServiceImpl implements BlogService {
@@ -21,7 +30,25 @@ public class BlogServiceImpl implements BlogService {
 
     @Override
     public Page<Blog> listBlog(Pageable pageable, Blog blog) {
-        return null;
+        return blogRepository.findAll(new Specification<Blog>() {
+            @Override
+            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> predicates = new ArrayList<>();
+                if (blog != null && !"".equals(blog.getTitle())) {
+                    predicates.add(criteriaBuilder.like(root.<String>get("title"), blog.getTitle()));
+                }
+
+                if (blog.getType() != null && blog.getType().getId() != null) {
+                    predicates.add(criteriaBuilder.equal(root.<Type>get("type").get("id"), blog.getType().getId()));
+                }
+                if(blog.isRecommend()){
+                    predicates.add(criteriaBuilder.equal(root.<Boolean>get("recommend"), blog.isRecommend()));
+                }
+                
+                criteriaQuery.where(predicates.toArray(new Predicate[predicates.size()]));
+                return null;
+            }
+        }, pageable);
     }
 
     @Override
