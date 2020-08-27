0a3b44072236f8201c3a2df35c12007c90465bdd
Aaron <1546848781@qq.com>
2020-08-27 13:01:37 +0800

    博主回复评论

--- src/main/java/com/al/blog/web/CommentController.java
+++ src/main/java/com/al/blog/web/CommentController.java
@@ -1,6 +1,7 @@
 package com.al.blog.web;
 
 import com.al.blog.po.Comment;
+import com.al.blog.po.User;
 import com.al.blog.service.BlogService;
 import com.al.blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class CommentController {
     @Autowired
@@ -19,7 +22,7 @@ public class CommentController {
 
     @Autowired
     private BlogService blogService;
-    
+
     @Value("${comment.avatar}")
     private String avatar;
 
@@ -30,10 +33,20 @@ public class CommentController {
     }
 
     @PostMapping("/comments")
-    public String post(Comment comment) {
+    public String post(Comment comment, HttpSession session) {
         Long blogId = comment.getBlog().getId();
         comment.setBlog(blogService.getBlog(blogId));
-        comment.setAvatar(avatar);
+        
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
+            comment.setAvatar(user.getAvatar());
+            comment.setAdminComment(true);
+            comment.setNickname(user.getNickname());
+        }else{
+            comment.setAvatar(avatar);
+            comment.setAdminComment(false);
+        }
+
         commentService.saveComment(comment);
         return "redirect:/comments/" + blogId;
     }
